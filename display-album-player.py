8c694736aa9eec7f611f35aebe3d48f11cd404ef
AlexMan123456 <alex.man03888@gmail.com>
2025-01-31 23:24:41 +0000

    Ignoring __pycache__

--- display-album-player.py
+++ display-album-player.py
@@ -5,5 +5,6 @@ class DisplayAlbumPlayer(Scene):
     def construct(self):
         filename = "play-album.js"
         code_window = Code(code_file=filename, language="Javascript", background="window").scale(0.75)
+        
 
         self.add(code_window)
