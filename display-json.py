5f1f90fe31f1dc8e62d538534c4c7ad564ae8b2d
AlexMan123456 <alex.man03888@gmail.com>
2025-02-01 00:38:05 +0000

    Changed font of display-json header

--- display-json.py
+++ display-json.py
@@ -4,8 +4,9 @@ from manim import *
 class DisplayJSON(Scene):
     def construct(self):
         filename = "neural-anthems.json"
-        code_window = Code(code_file=filename, language="Javascript", background="window").scale(0.5)
-        window_header = Text(filename, font_size=14).next_to(code_window, UP, buff=-0.25)
-        self.add_foreground_mobject(window_header)
+        code_window = VGroup(Code(code_file=filename, language="Javascript", background="window").scale(0.5))
+        code_window.add(Text(filename, font_size=14, font="Arial").next_to(code_window, UP, buff=-0.25))
 
-        self.add(window_header, code_window)
+        code_window.scale(1.15).shift(0.5*RIGHT+0.2*DOWN)
+
+        self.add(code_window)
